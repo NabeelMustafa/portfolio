d73712698d7e6147f40f496f3f588331bd309d55
NabeelMustafa <bitf14m016@pucit.edu.pk>
2019-01-18 16:41:18 +0500

    delete user model and db causing error with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_102703) do
+ActiveRecord::Schema.define(version: 2019_01_18_113114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,14 +69,6 @@ ActiveRecord::Schema.define(version: 2019_01_17_102703) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "blogs", "topics"
